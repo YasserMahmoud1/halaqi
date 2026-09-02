50f315fc9e0ce21ee608623e892a5cc2bcc227f8
YasserMahmoud1 <109506170+YasserMahmoud1@users.noreply.github.com>
2026-09-02 19:12:34 +0300

    Update Android Gradle plugin version
    Updated the Android Gradle plugin version to 8.11.1.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,9 +18,9 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.11.1" apply false // Update to 8.11.1 or higher
+    id "org.jetbrains.kotlin.android" version "2.1.0" apply false
 }
 
 include(":app")
